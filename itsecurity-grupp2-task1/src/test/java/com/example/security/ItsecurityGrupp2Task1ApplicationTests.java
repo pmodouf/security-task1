55a206852751b2a33a22215645a5a65c408df58b
Dmitri Gustafsson <dima.gustafsson@hotmail.com>
2023-08-23 09:49:09 +0200

    main

---
+++ itsecurity-grupp2-task1/src/test/java/com/example/security/ItsecurityGrupp2Task1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsecurityGrupp2Task1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
